963858e05055de52a871533247453428c9556310
Zach Gause <zach.gause@gmail.com>
2025-02-10 23:01:19 -0500

    Age Validation publication

--- src/TheaterKioskAgeValidation.java
+++ src/TheaterKioskAgeValidation.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
+public class TheaterKioskAgeValidation
+{
+    public static void main(String[] args)
+    {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner in = new Scanner(System.in);
+                int age = 0;
+                String trash = "";
+
+                System.out.print("Enter your age: ");
+
+                if (in.hasNextInt())
+                {
+                    age = in.nextInt();
+                    in.nextLine(); // Clear the buffer
+
+                    if (age >= 21)
+                    {
+                        System.out.println("You get a wristband.");
+                    }
+                    // else { do nothing }
+                } else
+                {
+                    trash = in.nextLine();
+                    System.out.println("Invalid input: " + trash);
+                    System.out.println("Please enter a valid integer for your age.");
+                }
     }
 }
